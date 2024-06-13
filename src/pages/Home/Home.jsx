f150d19ebf18aa896e11077ad79974a5456eb3c5
unknown <wilfredchong43@yahoo.com>
2024-06-13 10:29:34 +0800

    home-service v1.0

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,34 @@ import ComingSoon from "../../components/card/services/ComingSoon";
 import Footer from "../../components/footer/Footer";
 import 'swiper/swiper-bundle.css';
 import sisLogo from "../../assets/logo/sisLogo.png";
+import { MdOutlineWeb } from "react-icons/md";
+import { VscDebugConsole } from "react-icons/vsc";
+import { FaCoins } from "react-icons/fa";
+
+
+const services = [
+    {
+        id: 1,
+        symbol: <MdOutlineWeb />,
+        title: "IT Consulting",
+        context: "Web development, e Business card, web hosting for enterprise or personal use",
+        url: <Link to="/digitalcard">Learn more</Link>,
+    },
+    {
+        id: 2,
+        symbol: <VscDebugConsole />,
+        title: "Assignment Aid",
+        context: "Solving on complex assignment for CS/IT student with guidance",
+        url: <Link to="/CSAid">Learn more</Link>,
+    },
+    {
+        id: 3,
+        symbol: <FaCoins />,
+        title: "Unit Trust Consultancy",
+        context: "Unlock the potential of your savings by discover unit trusts offer to investors!",
+        url: <Link to="/UTConsult">Learn more</Link>,
+    }
+]
 
 export default function Home() {
 
@@ -32,12 +60,31 @@ export default function Home() {
                 <AssignmentHelper />
                 <ComingSoon />
             </div>
-        <div className="Contact-UT">
-            <h1>Feel free to reach me out for free consultation!</h1>
-                <Link to="/contact" className="contact-button">
-                    Contact Me!
-                </Link>
-        </div>
+            <div className="service-intro">
+                <h1>Our Services</h1>
+                <h2>We Provide a Wide Variety of Services</h2>
+                <p>We deliver top-notch solutions tailored to your needs, including professional websites, 
+                    expert unit trust advice, computer science assignment help, reliable website hosting, 
+                    and innovative e-digital business cards. Our expert team ensures you receive the highest quality service every time.</p>
+                <div className="service-arrangement">
+                    {
+                    services.map(data => (
+                        <div className="service-context" key={data.id}>
+                            <h1>{data.symbol}</h1>
+                            <h2>{data.title}</h2>
+                            <p>{data.context}</p>
+                            <p>{data.url}</p>
+                        </div>
+                    ))
+                }
+                </div>
+            </div>
+            <div className="Contact-UT">
+                <h1>Feel free to reach me out for free consultation!</h1>
+                    <Link to="/contact" className="contact-button">
+                        Contact Me!
+                    </Link>
+            </div>
             <Footer />
         </>
     )
