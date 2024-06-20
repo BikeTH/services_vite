5fb62bba79412461304a6b196ba8c421724130bf
Wilfred <700024165@student.curtin.edu.my>
2024-06-20 21:54:34 +0800

    about cyberpunk theme

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -13,22 +13,24 @@ export default function About() {
     return(
         <>
             <div className="about-title">
+                <div className="introduction">
                 <h1>ABOUT ME</h1>
-            </div>
-            <div className="about-introduction">
-                <p className="introduction"><ReactTyped style={{color:"#9fef00"}}strings={["Yo! You can call me ", "你好啊！可以叫我, ", "やあ! 私は "]}
+                <p><ReactTyped style={{color:"#9fef00"}}strings={["Yo! You can call me ", "你好啊！可以叫我, ", "やあ! 私は "]}
                     typeSpeed={100}
                     backSpeed={100}
                     loop/>
                     Wilfred,</p> 
-                <p className="introduction">I graduated from <span style={{color:"yellow"}}>Curtin University</span>, BSc Major in <span style={{color:"#9fef00"}}>CyberSecurity</span>.</p>
-                <p className= "introduction">If you're <span style={{color:"#9fef00"}}>looking</span> for a versatile team player who thrives in both the <span style={{color:"red"}}>security</span> realm and the <span style={{color:"#9fef00"}}>creative</span> landscape of Web development, 
+                <p>I graduated from <span style={{color:"yellow"}}>Curtin University</span>, BSc Major in <span style={{color:"#9fef00"}}>CyberSecurity</span>.</p>
+                <p>If you're <span style={{color:"#9fef00"}}>looking</span> for a versatile team player who thrives in both the <span style={{color:"red"}}>security</span> realm and the <span style={{color:"#9fef00"}}>creative</span> landscape of Web development, 
                 I'd love to <span style={{color:"cyan"}}>Connect</span> and discuss how I can contribute to your tech department's success.</p>
+                </div>
+            </div>
+            <div className="about-introduction">
                 <div className="about-experience">
                     <h1>My Experience</h1>
-                </div>
-                <div className="carousel-experience">
-                    <CarouselExp />
+                    <div className="carousel-experience">
+                        <CarouselExp />
+                    </div>
                 </div>
                 <div className="about-skilled">
                     <h1>Skills</h1>
